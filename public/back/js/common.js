535e4783ab101be517656ed8bed5d7b1a6db3ce1
smc <1135961843@qq.com>
2019-10-21 16:46:32 +0800

    添加进度条功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -4,3 +4,12 @@
 
 
 //开启进度条
+$(document).ajaxStart(function(){
+    NProgress.start();
+})
+
+$(document).ajaxStop(function(){
+    setTimeout(function(){
+        NProgress.done();
+    },1000);
+})
